e7f904289e0fb0924e226e86976617a8ca2a5187
ashwin135-jpg <ashwin.ganesh01@student.csulb.edu>
2025-12-06 20:29:57 -0800

    Update Home tool to include redirect and remove text
    Removed introductory text and added a redirect meta tag for the Home tool.

--- isa_app.py
+++ isa_app.py
@@ -44,13 +44,11 @@ lottie_common = load_lottieurl(lottie_url)
 
 if tool == "Home":
     st.markdown("""
-    ## ISA Master Tool
+        <meta http-equiv="refresh" content="0; url="https://ashwin135-jpg.github.io/ISA-ATM-Calculator/" />
+        <p>Redirecting...</p>
+    """, unsafe_allow_html=True)
+    st.stop()
 
-    This is the backend calculator app.
-
-    - Use the sidebar to choose a tool, or  
-    - [Click here to return to the main landing page](https://ashwin135-jpg.github.io/ISA-ATM-Calculator/)
-    """)
 
 elif tool == "ISA Atmosphere Calculator":
     isa_tool.render()
