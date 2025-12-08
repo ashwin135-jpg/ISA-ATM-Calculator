4f0911ad97ec143333c96a735f75bbc34c666fc0
ashwin135-jpg <ashwin.ganesh01@student.csulb.edu>
2025-12-07 22:46:19 -0800

    Fix typos and clean up redirection code

--- isa_app.py
+++ isa_app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 from streamlit_lottie import st_lottie
-import streamlit.components.v1 as components  # ⬅ make sure this is here
-
 from utils import load_lottieurl
 from tools import (
     isa_tool,
@@ -11,8 +9,6 @@ from tools import (
     mission_planner_tool,
     city_to_city_tool,
 )
-
-GITHUB_HOME = "https://ashwin135-jpg.github.io/ISA-ATM-Calculator/"
 
 # -------------------------
 # Sidebar / query param setup
@@ -24,7 +20,7 @@ tool_options = [
     "Lift and Drag Calculator",
     "Fuel Consumption & Range Estimator",
     "Mission Planner",
-    "City to City Flight Estimator",
+    "City-to-City Flight Estimator",
 ]
 
 default_tool = st.query_params.get("tool", "ISA Atmosphere Calculator")
@@ -45,40 +41,24 @@ st.session_state["tool"] = tool
 st.query_params["tool"] = tool
 
 # -------------------------
-# Lottie animation (one for now)
+# Lottie animation for tool pages
 # -------------------------
 lottie_url = "https://lottie.host/68ecc80f-3865-4071-89bf-1db845e65c6e/O67It7eqk8.json"
 lottie_common = load_lottieurl(lottie_url)
+
+if lottie_common and tool != "Home":
+    st_lottie(lottie_common, height=250, key=tool.replace(" ", "_"))
 
 # -------------------------
 # Routing
 # -------------------------
 if tool == "Home":
-    # Render a tiny HTML page whose only job is to redirect
-    components.html(
-        f"""
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <meta http-equiv="refresh" content="0; url={GITHUB_HOME}" />
-            <script type="text/javascript">
-                // Try JS redirect
-                window.top.location.href = "{GITHUB_HOME}";
-            </script>
-        </head>
-        <body>
-            <p>
-                Redirecting to
-                <a href="{GITHUB_HOME}" target="_top">
-                    ISA Master Tool Homepage
-                </a>...
-            </p>
-        </body>
-        </html>
+    st.markdown(
+        """
+        <meta http-equiv="refresh" content="0; url='https://ashwin135-jpg.github.io/ISA-ATM-Calculator/'" />
         """,
-        height=100,
+        unsafe_allow_html=True,
     )
-    st.stop()
 
 elif tool == "ISA Atmosphere Calculator":
     isa_tool.render()
@@ -95,5 +75,5 @@ elif tool == "Fuel Consumption & Range Estimator":
 elif tool == "Mission Planner":
     mission_planner_tool.render()
 
-elif tool == "City to City Flight Estimator":
+elif tool == "City-to-City Flight Estimator":
     city_to_city_tool.render()
